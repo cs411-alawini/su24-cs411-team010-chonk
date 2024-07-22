7b35b8e76a0e68f1e6c1f9c7b451ec00e3ca5a72
Daniel Wang <hello@danielwang.dev>
2024-07-22 18:26:49 -0400

    Scaffold frontend and backend directories

---
+++ backend/tests/test_api.py
@@ -0,0 +1,16 @@
+from backend.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+
+def test_read_item():
+    response = client.get("/items/5")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 5, "q": None}
