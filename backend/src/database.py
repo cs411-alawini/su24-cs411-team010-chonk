7d295b68c04c0502ad73cc52fcee554b171011d8
Daniel Wang <hello@danielwang.dev>
2024-07-22 23:36:59 -0400

    Add initial GCP database integration

---
+++ backend/src/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from config import settings
+
+
+from sqlalchemy import URL
+
+if settings.database_username is None:
+    raise ValueError("database_username is required")
+
+if settings.database_dbname is None:
+    raise ValueError("database_dbname is required")
+
+if settings.database_host is None:
+    raise ValueError("database_host is required")
+
+if settings.database_password is None:
+    raise ValueError("database_password is required")
+
+database_url = URL.create(
+    "mysql+pymysql",
+    username=settings.database_username,
+    password=settings.database_password.get_secret_value(),
+    host=settings.database_host,
+    database=settings.database_dbname,
+)
+
+engine = create_engine(database_url, pool_size=20, max_overflow=0, pool_pre_ping=True)
