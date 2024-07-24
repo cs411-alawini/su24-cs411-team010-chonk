9a606adcf899cc0c3eb681b0b2cb07b7b019b806
FPynk <acloh2@illinois.edu>
2024-07-23 22:28:20 -0500

    removed link

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import OwnStats from './OwnStats';
