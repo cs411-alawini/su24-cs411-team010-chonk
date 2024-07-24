b21b0f89bcd4583cbc59d1a2f64a2bade08e23b4
Acelynn Zhang <azyx0987e@gmail.com>
2024-07-24 17:33:33 -0500

    2routes+scrape

--- scrapedata/download.py
+++ scrapedata/download.py
@@ -1,19 +1,20 @@
 import re
-from datetime import datetime, timezone
 import time
-
 from collections import defaultdict
+from datetime import datetime, timezone
 
 import requests
 from selectolax.parser import HTMLParser
 
 
 def vlr_match_results_in_depth(startpg, endpg):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+    }
     with requests.Session() as session:
         pgcount = startpg
         session.headers.update(headers)
-        params= {'page': str(pgcount)}
+        params = {"page": str(pgcount)}
         url = "https://www.vlr.gg/matches/results"
         result = []
 
@@ -32,22 +33,22 @@ def vlr_match_results_in_depth(startpg, endpg):
                 url_path = item.attributes["href"]
 
                 result.append(url_path)
-        
-            pgcount +=1
-            params['page'] = str(pgcount)
+
+            pgcount += 1
+            params["page"] = str(pgcount)
             print(params)
             resp = session.get(url, params=params)
             html = HTMLParser(resp.text)
             status = resp.status_code
             time.sleep(0.1)
-        
+
         for idx, game in enumerate(result):
             resp = session.get(f"https://www.vlr.gg{game}", headers=headers)
             print(f"https://www.vlr.gg{game}")
             with open(f"./downloadpgs/{idx}.html", "w") as curr:
                 curr.write(resp.text)
             time.sleep(0.3)
-            
+
 
 vlr_match_results_in_depth(1, 500)
 print("done!")
